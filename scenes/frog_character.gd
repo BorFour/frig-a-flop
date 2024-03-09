71d7cdf5e30d194764409487127ff282fbe7ef67
Borja Fourquet <b.fourquet@outlook.es>
2024-03-09 13:10:47 +0100

    💫 Add "back to standing" animation + minimum initial jump force

--- scenes/frog_character.gd
+++ scenes/frog_character.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 10
-const ROTATE_VELOCITY = 20
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 10
+const ROTATE_VELOCITY: float = 20
+const MIN_JUMP_FORCE: float = 0.5
+const MIN_STANDING_X_ROTATION_OFFSET: float = 0.02
+const BACK_TO_STANDING_SPIN_VELOCITY: float = 10
 var jump_force: float = 0
 var spin_velocity: float = 0
 
@@ -20,8 +23,12 @@ func _physics_process(delta):
 
 	# Handle jump
 	if Input.is_action_just_released("jump") and is_on_floor():
-		print("Jump! with %.2f" % jump_force)
-		velocity.y = JUMP_VELOCITY * jump_force
+		if jump_force < MIN_JUMP_FORCE:
+			print("Not enough jump force %.2f" % jump_force)
+			jump_force = 0
+		else:
+			print("Jump! with %.2f" % jump_force)
+			velocity.y = JUMP_VELOCITY * jump_force
 
 	# Handle charge jump
 	if Input.is_action_pressed("jump") and is_on_floor():
@@ -30,24 +37,25 @@ func _physics_process(delta):
 		jump_force = 0
 
 	# Handle rotation
-	var jump_rotation = -delta * ROTATE_VELOCITY / 10
+	var base_rotation = -delta * ROTATE_VELOCITY / 10
 
 	if is_on_floor_only():
-		spin_velocity = 0
+		base_rotation = 0
+		if abs(rotation.x) > MIN_STANDING_X_ROTATION_OFFSET:
+			spin_velocity = (
+				BACK_TO_STANDING_SPIN_VELOCITY
+				 if rotation.x > 0
+				else -BACK_TO_STANDING_SPIN_VELOCITY
+			)
+		else:
+			spin_velocity = 0
 	elif Input.is_action_pressed("front_spin"):
 		spin_velocity += 1
-		jump_rotation *= 3
+		base_rotation *= 3
 	elif Input.is_action_pressed("back_spin"):
 		spin_velocity = max(0, spin_velocity - 3)
 	else:
 		spin_velocity = max(0, spin_velocity - 1)
 	
-	if not is_on_floor():
-		rotate_x(jump_rotation - spin_velocity * 0.003)
-	elif is_on_floor_only():
-		spin_velocity = 0
-		#rotation.x = 0
-		#position = Vector3(0, 0, 0)
-		rotate_toward(0, 0, 0)
-
+	rotate_x(base_rotation - spin_velocity * 0.003)
 	move_and_slide()
