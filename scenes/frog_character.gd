dccd8f379f4ce077e7d19e3fed6b5e8052bc2e37
Borja Fourquet <b.fourquet@outlook.es>
2024-03-16 12:59:25 +0100

    ✨ Add flip counter logic and placeholder HUD element

--- scenes/frog_character.gd
+++ scenes/frog_character.gd
@@ -16,11 +16,22 @@ var spin_velocity: float = 0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
  
+# Variables to for flip counter
+const FLIP_COUNT_MARGIN_ANGLE: float = -PI/4  # if a rotation is missing this angle, we still count it
+var total_jump_rotation: float = FLIP_COUNT_MARGIN_ANGLE
+var current_jump_flip_count: int = 0
+var last_jump_flip_count: int = 0
 
 func _physics_process(delta):
-	# Add the gravity
+	# Add the gravity & calculate flips
 	if not is_on_floor():
-		velocity.y -= gravity * delta 
+		velocity.y -= gravity * delta
+		current_jump_flip_count = abs(total_jump_rotation / (2 * PI))
+		last_jump_flip_count = current_jump_flip_count
+	else:
+		# Reset the flip counter if the character is on the floor
+		current_jump_flip_count = 0
+		total_jump_rotation = FLIP_COUNT_MARGIN_ANGLE
 
 	# Handle jump
 	if Input.is_action_just_released("jump") and is_on_floor():
@@ -28,7 +39,7 @@ func _physics_process(delta):
 			print("Not enough jump force %.2f" % jump_force)
 			jump_force = 0
 		else:
-			print("Jump! with %.2f" % jump_force)
+			print("Jump! with force of %.2f" % jump_force)
 			velocity.y = JUMP_VELOCITY * jump_force
 
 	# Handle charge jump
@@ -59,5 +70,10 @@ func _physics_process(delta):
 	else:
 		spin_velocity = max(0, spin_velocity - 1)
 	
-	rotate_x(base_rotation - spin_velocity * 0.003)
+	# Rotate and update total jump rotation
+	var final_step_rotation = base_rotation - spin_velocity * 0.003
+	total_jump_rotation += final_step_rotation
+	rotate_x(final_step_rotation)
+	
+	# Needed by the physics engine I think
 	move_and_slide()
