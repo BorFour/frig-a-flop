3439f256a54e81cb90fb13363925afc8b8d8c0b6
Borja Fourquet <b.fourquet@outlook.es>
2024-03-18 12:55:32 +0100

    🎨 Target node for `hud_text_value.gd` as `@export` variable

--- prefabs/hud_text_value.gd
+++ prefabs/hud_text_value.gd
@@ -1,7 +1,7 @@
 extends Label
 
 
-@onready var target: Node3D = $"/root/MainScene/FrogCharacter"
+@export var target: Node3D
  
 # Called when the node enters the scene tree for the first time.
 func _ready():
